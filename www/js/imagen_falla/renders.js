95a5ded2af476195a538b130598bec689330eeae
maoriab <maorimichell788@gmail.com>
2026-07-15 17:30:45 -0500

    Update renders.js

--- www/js/imagen_falla/renders.js
+++ www/js/imagen_falla/renders.js
@@ -9,7 +9,16 @@ export default function renderImagenes(imagenes) {
                 <img src="${i.url_archivo}" alt="Falla" style="width: 50px; height: 50px; object-fit: cover; border-radius: 4px; cursor: pointer;">
             </a>
             </td>
-            <td>${i.fecha_subida}</td>
+            <td>
+    ${new Date(i.fecha_subida.replace(' ', 'T') + 'Z').toLocaleString('es-MX', { 
+        year: 'numeric', 
+        month: '2-digit', 
+        day: '2-digit', 
+        hour: '2-digit', 
+        minute: '2-digit',
+        timeZone: 'America/Cancun' 
+    })}
+</td>
             <td>
                 <button data-id="${i.id_imagen}" class="editBtn btn btn-sm btn-warning">Editar</button>
                 <button data-id="${i.id_imagen}" class="deleteBtn btn btn-sm btn-error">Eliminar</button>
